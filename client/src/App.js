9049c411b2e5a28bfc7003de2cf20354d2ffef3a
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2026-06-28 17:44:11 +0530

    Started creating landing page after adding boilerplates

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import Main from './pages/Main';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif', backgroundColor: '#FFFFFF', minHeight: '100vh' }}>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
